6f730813f22a9d356f4ac0fa3aa61ba32812f905
Serhii Maltsev <83024347+smaltsev-v@users.noreply.github.com>
2022-02-06 19:31:28 +0200

    1

--- phonebook/src/components/ContactList/ContactUser/ContactUser.jsx
+++ phonebook/src/components/ContactList/ContactUser/ContactUser.jsx
@@ -1,26 +1,25 @@
-// import React from "react";
-import PropTypes from "prop-types";
-import s from './ContactUser.module.css'
+// import PropTypes from "prop-types";
+// import s from './ContactUser.module.css'
 
 
-const ContactUser = ({ id, name, number, onDeleteContacts }) => {
-  return (
-    <li className={s.contactItem}>
-      <span className={s.name}>{name}</span>
-      <span className={s.phone}>{number}</span>
-      <button
-        className={s.btnDelete}
-        type="button"
-        onClick={() => onDeleteContacts(id)}
-      >
-        Delete
-      </button>
-    </li>
-  );
-}
+// const ContactUser = ({ id, name, number, onDeleteContacts }) => {
+//   return (
+//     <li className={s.contactItem}>
+//       <span className={s.name}>{name}</span>
+//       <span className={s.phone}>{number}</span>
+//       <button
+//         className={s.btnDelete}
+//         type="button"
+//         onClick={() => onDeleteContacts(id)}
+//       >
+//         Delete
+//       </button>
+//     </li>
+//   );
+// }
 
-export default ContactUser;
+// export default ContactUser;
 
-ContactUser.propTypes = {
-  onDeleteContacts: PropTypes.func.isRequired,
-};
+// ContactUser.propTypes = {
+//   onDeleteContacts: PropTypes.func.isRequired,
+// };
