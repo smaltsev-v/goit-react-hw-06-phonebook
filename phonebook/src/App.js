ca6e6c89e43d284eadc046dc259579f6eddc461c
Serhii Maltsev <83024347+smaltsev-v@users.noreply.github.com>
2022-02-08 19:47:48 +0200

    fix

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,26 +1,24 @@
 import ContactForm from './components/ContactForm';
 import ContactList from './components/ContactList';
 import Filter from './components/Filter';
-import {Provider} from 'react-redux';
-import {store} from './redux/store'
+
+
 import './App.css'
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="container" >
-        <h1 className="title__h"><span className='title__p'>Phone</span>book</h1>
-        <ContactForm/>
-        <h2 className="title__contact">Contacts</h2>
-        <Filter/>
-        <ContactList/>
-        <ToastContainer
-          autoClose={3700}
-        />
-      </div>
-    </Provider>
+    <div className="container" >
+      <h1 className="title__h"><span className='title__p'>Phone</span>book</h1>
+      <ContactForm />
+      <h2 className="title__contact">Contacts</h2>
+      <Filter />
+      <ContactList />
+      <ToastContainer
+        autoClose={3700}
+      />
+    </div>
   );
 }
 
